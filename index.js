bb6be482f7070ac1df848a395aa65f7132340680
michald <michal.drobniak@schibsted.pl>
2015-02-27 16:12:31 +0100

    Delete and put methods added

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+'use strict';
 var Hapi = require('hapi');
 var Good = require('good');
 var config = require('./config/global');
--- routes/events.js
+++ routes/events.js
@@ -4,45 +4,70 @@ var Joi = require('joi');
 var _ = require('lodash');
 var db = require('./../db');
 
-var Events = {
-    getAll: function(req, reply) {
-        db.knex.select().from('events')
-            .then(function(rows) {
-                reply(rows);
-            })
-            .catch(function(err) {
-                console.log('DB error: ' + err.stack);
-                reply(Boom.create(503, 'DB error'));
-            })
-    },
-    get: function(req, reply) {
-        var id = req.params.id, record;
-        db.knex.select().from('events').where({id: id})
-            .then(function(rows) {
-                if(rows.length === 1) {
-                    reply(rows[0]);
-                } else {
-                    reply(Boom.notFound('Event with id: ' + id + ' not found'));
-                }
-            })
-            .catch(function(err) {
-                console.log('DB error: ' + err.stack);
-                reply(Boom.create(503, 'DB error'));
-            });
-    },
-    create: function(req, reply) {
-        var object = req.payload;
-        db.knex.insert(object).into('events')
-            .then(function(rows) {
-                console.log(rows);
-                reply({insertedId: rows[0]}).code(201);
-            })
-            .catch(function(err) {
-                console.log('DB error: ' + err.stack);
-                reply(Boom.create(503, 'DB error'));
-            });
-    }
-};
+var Events = (function() {
+    var table = 'events';
+    return {
+        getAll: function(req, reply) {
+            db.knex.select().from(table)
+                .then(function(rows) {
+                    reply(rows);
+                })
+                .catch(function(err) {
+                    console.log('DB error: ' + err.stack);
+                    reply(Boom.create(503, 'DB error'));
+                });
+        },
+        get: function(req, reply) {
+            var id = req.params.id;
+            db.knex.select().from(table).where({id: id})
+                .then(function(rows) {
+                    if (rows.length === 1) {
+                        reply(rows[0]);
+                    } else {
+                        reply(Boom.notFound('Event with id: ' + id + ' not found'));
+                    }
+                })
+                .catch(function(err) {
+                    console.log('DB error: ' + err.stack);
+                    reply(Boom.create(503, 'DB error'));
+                });
+        },
+        create: function(req, reply) {
+            var object = req.payload;
+            db.knex.insert(object).into(table)
+                .then(function(rows) {
+                    reply({insertedId: rows[0]}).code(201);
+                })
+                .catch(function(err) {
+                    console.log('DB error: ' + err.stack);
+                    reply(Boom.create(503, 'DB error'));
+                });
+        },
+        delete: function(req, reply) {
+            var id = req.params.id;
+            db.knex.del().from(table).where({id: id})
+                .then(function(affectedRows) {
+                    reply({deletedRows: affectedRows}).code(200);
+                })
+                .catch(function(err) {
+                    console.log('DB error: ' + err.stack);
+                    reply(Boom.create(503, 'DB error'));
+                });
+        },
+        update: function(req, reply) {
+            var id = req.params.id,
+                object = req.payload;
+            db.knex(table).update(object).where({id: id})
+                .then(function(affectedRows) {
+                    reply({updatedRows: affectedRows}).code(200);
+                })
+                .catch(function(err) {
+                    console.log('DB error: ' + err.stack);
+                    reply(Boom.create(503, 'DB error'));
+                });
+        }
+    };
+})();
 
 var plugin = {
     register: function(server, options, next) {
@@ -51,7 +76,6 @@ var plugin = {
             path: '/api/v1/events',
             handler: Events.getAll
         });
-
         server.route({
             method: 'GET',
             path: '/api/v1/events/{id}',
@@ -64,7 +88,6 @@ var plugin = {
                 }
             }
         });
-
         server.route({
             method: 'POST',
             path: '/api/v1/events',
@@ -72,12 +95,42 @@ var plugin = {
             config: {
                 validate: {
                     payload: {
-                        name: Joi.string().min(2),
-                        place: Joi.string().min(1)
+                        name: Joi.string().min(2).required(),
+                        place: Joi.string().min(1).required()
                     }
                 }
             }
-        })
+        });
+        server.route({
+            method: 'DELETE',
+            path: '/api/v1/events/{id}',
+            handler: Events.delete,
+            config: {
+                validate: {
+                    params: {
+                        id: Joi.number().integer().min(0)
+                    }
+                }
+            }
+        });
+        server.route({
+            method: 'PUT',
+            path: '/api/v1/events/{id}',
+            handler: Events.update,
+            config: {
+                validate: {
+                    payload: {
+                        name: Joi.string().min(2),
+                        place: Joi.string().min(1)
+                    },
+                    params: {
+                        id: Joi.number().integer().min(0)
+                    }
+                }
+            }
+        });
+
+        next();
     }
 };
 plugin.register.attributes = {
